ca7cc76ec7f4d5c482d07613384d32711e1e2df9
vergarapog <vbrian.dev@gmail.com>
2024-10-31 15:52:06 +0800

    finish exe 8.6

--- exercise1/index.js
+++ exercise1/index.js
@@ -1,5 +1,7 @@
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
+const { v1: uuid } = require("uuid");
+
 
 let authors = [
   {
@@ -101,7 +103,7 @@ const typeDefs = `
  type Author {
     name: String!
     id: String!
-    born: Int!
+    born: Int
     bookCount: Int
   }
 
@@ -118,6 +120,15 @@ const typeDefs = `
     authorCount: Int!
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
+  }
+
+  type Mutation {
+    addBook (
+      title: String!
+      published: Int!
+      author: String!
+      genres: [String!]!
+    ): Book
   }
 `;
 
@@ -142,6 +153,20 @@ const resolvers = {
   Author: {
     bookCount: (root) => {
       return books.reduce((accu, curr) => (root.name === curr.author ? accu + 1 : accu), 0);
+    }
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      const newBook = {...args, id: uuid()}
+
+      books = books.concat(newBook);
+
+      if(!authors.find(author => author.name === args.author)) {
+        const newAuthor = {name: args.author, id: uuid() }
+        authors = authors.concat(newAuthor)
+      }
+
+      return newBook
     }
   }
 };
