c8dcaeedf3ea6b7dadcfbbee32b9edd1223267b1
vergarapog <vbrian.dev@gmail.com>
2024-11-01 22:31:28 +0800

    implement useQuery

--- frontend-sample/src/App.tsx
+++ frontend-sample/src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { gql, useQuery } from "@apollo/client";
+
+const ALL_PERSONS = gql`
+  query {
+    allPersons {
+      name
+      phone
+      id
+    }
+  }
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const result = useQuery(ALL_PERSONS);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  if (result.loading) {
+    return <div>Loading....</div>;
+  }
+
+  return <div>{result.data.allPersons.map((p) => p.name).join(", ")}</div>;
 }
 
-export default App
+export default App;
--- frontend-sample/src/main.tsx
+++ frontend-sample/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
-createRoot(document.getElementById('root')!).render(
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </StrictMode>
+);
