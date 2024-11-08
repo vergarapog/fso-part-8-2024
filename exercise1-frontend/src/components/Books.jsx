902fdf8470de5827bc3ad989ceb8fab8e19fde54
vergarapog <vbrian.dev@gmail.com>
2024-11-08 17:13:20 +0800

    finished exe 8.9

--- exercise1-frontend/src/components/Books.jsx
+++ exercise1-frontend/src/components/Books.jsx
@@ -1,9 +1,21 @@
+import { useQuery } from "@apollo/client"
+import {  ALL_BOOKS } from "../queries"
+
 const Books = (props) => {
+  const result = useQuery(ALL_BOOKS)
+  
+  // eslint-disable-next-line react/prop-types
   if (!props.show) {
     return null
   }
 
-  const books = []
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+
+  const books = result.data.allBooks;
+
+  console.log(books)
 
   return (
     <div>
