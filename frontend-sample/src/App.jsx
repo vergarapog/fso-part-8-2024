e5fb15e7a199eef16b2d065609a53f369c35af54
vergarapog <vbrian.dev@gmail.com>
2024-12-03 16:52:11 +0800

    add user login

--- frontend-sample/src/App.jsx
+++ frontend-sample/src/App.jsx
@@ -1,9 +1,10 @@
-import { gql, useQuery } from '@apollo/client'
+import { gql, useApolloClient, useQuery } from '@apollo/client'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import { useState } from 'react'
 import Notify from './components/Notify'
 import PhoneForm from './components/PhoneForm'
+import LoginForm from './components/LoginForm'
 
 const ALL_PERSONS = gql`
   query {
@@ -16,24 +17,47 @@ const ALL_PERSONS = gql`
 `
 
 const App = () => {
+  const [token, setToken] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
   const result = useQuery(ALL_PERSONS)
-
-  
-  const notify = (message) => {
-    setErrorMessage(message)
-    setTimeout(() => {
-      setErrorMessage(null)
-    }, 10000)
-  }
+  const client = useApolloClient()
 
   if (result.loading) {
     return <div>loading...</div>
   }
 
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 5000)
+  }
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
+
+  if(!token) {
+    return (
+      <div>
+        <Notify errorMessage={errorMessage} />
+        <h2>Login</h2>
+        <LoginForm 
+          setToken={setToken}
+          setError={notify}
+        />
+      </div>
+    )
+  }
+
+ 
+
   return (
     <div>
       <Notify errorMessage={errorMessage} />
+      <button onClick={logout}>logout</button>
       <PersonForm setError={notify} />
       <Persons persons={result.data.allPersons} />
       <PhoneForm setError={notify} />
